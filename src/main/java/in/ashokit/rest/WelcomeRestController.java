7d83f6d4a197cbb388ed2104ab794c5d91316dd0
OM SANKATA MOCHANA PANDA <pandaomsankatamochana123@gmail.com>
2025-07-31 12:54:40 +0530

    WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -35,6 +35,7 @@ public class WelcomeRestController {
 
 	public void m2() {
 		logger.info("***** m2() execution start *****");
+		String name="Om Sankata Mochana Panda";
 		String msg = "This is a test method for git collaboration";
 		logger.info("***** m2() execution end was happen now *****");
 		System.out.println(msg);
